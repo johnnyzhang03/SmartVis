138dbad3896b2016f97bdc2f4dbc45374d6282cd
Zhiming Zhang <zzm_0323@berkeley.edu>
2023-11-08 20:51:27 +0800

    Move repo to jupyterlab_apod directory

---
+++ labextension/static/lib_index_js.cb6b6c9c7f1f8319d834.js
@@ -0,0 +1,31 @@
+"use strict";
+(self["webpackChunkjupyterlab_apod"] = self["webpackChunkjupyterlab_apod"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/**
+ * Initialization data for the jupyterlab_apod extension.
+ */
+const plugin = {
+    id: 'jupyterlab_apod:plugin',
+    description: 'A JupyterLab extension.',
+    autoStart: true,
+    activate: (app) => {
+        console.log('JupyterLab extension jupyterlab_apod is activated!');
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.cb6b6c9c7f1f8319d834.js.map
